488201f72c6e3faf74414c07fc073faf20c58501
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2023-12-21 15:11:36 +0100

    psuh

--- react-snake-finished/src/GameOverModal.js
+++ react-snake-finished/src/GameOverModal.js
@@ -1,19 +1,36 @@
 // GameOverModal.js
-import React from "react";
+import React,  { useState } from "react";
 import "./css/style.css";
 
 const GameOverModal = ({ score, startGame }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const closeModal = () => {
+    setIsVisible(false);
+  };
+
+  const handlePlayAgain = () => {
+    setIsVisible(false);
+    startGame();
+  };
+
   return (
-    <div className="modal">
-      <div className="modal--content">
-        <h2>GAME OVER</h2>
-        <p>Your Score: {score}</p>
-        <div className="modal--footer">
-          <button className="m-btn btn--primary" onClick={startGame}>Play Again</button>
-          <button className="m-btn btn--outline" >Close</button>
+    isVisible && (
+      <div className="modal">
+        <div className="modal--content">
+          <h2>GAME OVER</h2>
+          <p>Your Score: {score}</p>
+          <div className="modal--footer">
+            <button className="m-btn btn--primary" onClick={handlePlayAgain}>
+              Play Again
+            </button>
+            <button className="m-btn btn--outline" onClick={closeModal}>
+              Close
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
